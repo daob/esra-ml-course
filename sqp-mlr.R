6d5b4d458efddaf23f694656791489c9d982ef5c
Daniel Oberski <daniel.oberski@gmail.com>
2019-07-12 12:21:19 +0000

    tweaks to simple example; faster default CV and random tuning settings

--- sqp-mlr.R
+++ sqp-mlr.R
@@ -35,6 +35,7 @@ sqp_matrix_test <- sqp_matrix[idx_test, ]
 
 
 ######### Simple analysis #########
+# See exercise 1.3
 
 # First, let's train a simple model without tuning on 
 #   the whole dataset
@@ -46,24 +47,30 @@ task <- sqp_matrix_train %>% select(-val) %>%
 # Define learner
 #   For the full list of available learners in mlr, see:
 # https://mlr.mlr-org.com/articles/tutorial/integrated_learners.html
-lrn <- makeLearner("regr.ranger")
+lrn <- makeLearner("regr.lm") 
 
 # Fit (train) model
-system.time(mod <- train(lrn, task))
+system.time( # This will show how long it takes, gives an idea of CV timing
+  mod <- train(lrn, task)
+)
 
 # Predict test values
-rel_predicted <- predict(mod, newdata = sqp_matrix_test)
+rel_predicted_test <- predict(mod, newdata = sqp_matrix_test)
+
+# Predict train values
+rel_predicted_train <- predict(mod, newdata = sqp_matrix_train)
 
 # You can now examine how well test data are predicted, 
 #   for example:
-plot(rel_predicted$data)
+plot(rel_predicted_test$data)
 
 # Or on original reliability scale:
-plot(rel_predicted$data %>% mutate_all(plogis))
+plot(rel_predicted_test$data %>% mutate_all(plogis))
 
 # mlr automatically calculates interesting performance measures
-#   for regression, the default is mean squared error (mse)
-performance(rel_predicted, measures = rsq)
+#   for regression, the default is mean squared error (mse). 
+# Here we ask for root-mean squared error and R-squared:
+performance(rel_predicted_test, measures = list(rmse, rsq))
 
 
 # To see which other measures are available:
@@ -114,7 +121,7 @@ ps_gbm <- makeParamSet(
 ## Choose a resampling strategy
 
 # Here 10-fold cross-validation is chosen:
-rdesc <- makeResampleDesc("CV", iters = 10L)
+rdesc <- makeResampleDesc("CV", iters = 3L)
 
 # To see which other options are available
 # (e.g. bootstrap, holdout, blocked/stratified CV, ...):
@@ -124,7 +131,7 @@ rdesc <- makeResampleDesc("CV", iters = 10L)
 meas <- rmse
 
 # Choose a tuning method. Here we use random sampling
-ctrl <- makeTuneControlRandom(maxit = 10L)
+ctrl <- makeTuneControlRandom(maxit = 3L)
 
 # aside from the standard grid search,
 # mlr provides a number of very advanced tuning methods, e.g.
@@ -165,25 +172,22 @@ tuned_fnn <- makeTuneWrapper(learner = "regr.fnn",
 
 # Combine all candidate learners into a list
 lrns <- list(makeLearner("regr.lm"), # Also linear regression
-             tuned_ksvm, tuned_gbm, tuned_rf)
-
-# For demonstration purposes create a list that only 
-#    includes random forests and linear regression
-#    to speed things up..
-faster_lrns <- list(makeLearner("regr.lm"), tuned_fnn)
-
+             tuned_fnn, tuned_ksvm, tuned_gbm, tuned_rf)
 
 ## Conduct the benchmark experiment
 
 # This can take a very long time!
-
-bmr <- benchmark(
-  learners = faster_lrns, # Replace w lrns if you enjoy waiting
-  tasks = rel_regr_task,
-  resamplings = rdesc,
-  # We'll also include some other measures of performance:
-  measures = list(rmse, mae, medae, rsq), 
-  show.info = TRUE)
+#  depending on number of CV folds and makeTuneControlRandom maxit
+# with k = 3 and maxit = 3 it should take about 120s
+system.time(
+  bmr <- benchmark(
+    learners = lrns,
+    tasks = rel_regr_task,
+    resamplings = rdesc,
+    # We'll also include some other measures of performance:
+    measures = list(rmse, mae, medae, rsq), 
+    show.info = TRUE)
+)
 
 ## Evaluate performance of various models
 
@@ -194,24 +198,84 @@ getBMRAggrPerformances(bmr)
 plotBMRBoxplots(bmr, measure = rsq)
 
 
-## Fit final chosen model, and look at importnace, effects
+####### Tune and train the chosen model ####### 
 
-learner_rf <- makeLearner("regr.ranger") %>% 
-  setHyperPars(num.trees = 520)
+# After choosing a model, we can examine the performance 
+#  in the experiment, or repeat the tuning for all folds
+#  to tune the selected model using resampling
 
-model_rf <- train(learner_rf, rel_regr_task)
+# (Note that it is also possible to perform the 
+#  model selection and tuning in one step, 
+#  using makeModelMultiplexer(). 
+#  See: https://mlr.mlr-org.com/reference/makeModelMultiplexer.html)
+
+# This time we'll also tune the mtry parameter
+ps_rf_ext <- makeParamSet(
+  # With more time choose upper 2000+
+  makeIntegerParam("num.trees", lower = 1L, upper = 500L),
+  # I've chosen the upper as sqrt(p) but with more time choose p
+  makeIntegerParam("mtry", lower = 1L, upper = sqrt(ncol(sqp_matrix)))
+)
+
+res <- tuneParams(learner = "regr.ranger",
+                  task = rel_regr_task, 
+                  # With more time choose 10 or bootstrap
+                  resampling = makeResampleDesc("CV", iters = 3L), 
+                  measures = rmse,
+                  par.set = ps_rf_ext, 
+                  # With more time choose genetic algortihm or large maxit
+                  control = makeTuneControlRandom(maxit = 5),
+                  show.info = TRUE)
+
+# Fit final chosen model with optimal hyperparameters:
+learner_rf <- makeLearner("regr.ranger", importance = "permutation") %>% 
+  setHyperPars(num.trees = res$x$num.trees, mtry = res$x$mtry)
+
+model_final <- train(learner_rf, rel_regr_task)
+
+
+###### Model summaries ###### 
+
+# Evaluate final error using hold test data
+# If we did not overfit using the cross-validation folds above, 
+#  this should give rmse similar to that shown by tuning
+predictions_test_final <- predict(model_final, newdata = sqp_matrix_test)
+performance(predictions_test_final, measures = list(rmse, rsq))
 
 # Generate a partial dependence plot for two features
 #  see here for more information:
 #  https://christophm.github.io/interpretable-ml-book/pdp.html
 pd_main <- generatePartialDependenceData(
-  model_rf, rel_regr_task, c("fixrefpoints", "ncategories") 
+  model_final, rel_regr_task, c("fixrefpoints", "ncategories") 
 )
 
 # Plot the result using mlr's built-in plotting method:
 plotPartialDependence(pd_main)
 
-# Show feature importance for random forest
-feature_importance <- getFeatureImportance(model_rf)
+# Show feature importance (only for random forest!)
+feature_importance <- getFeatureImportance(model_final)
 feature_importance
 
+# Below I make a plot of the top-n features in terms of their
+#   permutation importance - 
+# see https://christophm.github.io/interpretable-ml-book/feature-importance.html
+# Note this type of "importance" was criticized by Strobl (2016)
+
+n <- 20  # Top-n 
+
+# Get features and their "importance"
+topn <- feature_importance$res %>% 
+  sort(decreasing = TRUE) %>% .[1:n] %>% unlist %>% rev
+
+# Adjust plot spacing to longest name of feature
+spc <- names(topn) %>% sapply(nchar) %>% max(.)/2
+par(mar = c(5, spc, 2, 1))
+
+# Use base R graphics to plot importance
+topn %>% 
+  plot(., 1:n, pch = 16, col = "steelblue", 
+       axes = FALSE, ylab = "", xlab = "Permutation importance",
+       main = sprintf("Top %d features", n), xlim=c(0, max(topn)))
+axis(2, at = 1:n, labels = names(topn), las = 2, cex.axis = 0.8)
+axis(1, at = round(c(min(topn), median(range(topn)), max(topn)), 3))
+segments(rep(0, n), 1:n, topn, 1:n, col = "#44444433")
